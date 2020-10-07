420cddf8d6172d95547b48d4cb81a99041be74e0
megacenterr <68357857+megacenterr@users.noreply.github.com>
2020-10-07 11:56:12 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 clear
 wget https://raw.githubusercontent.com/megacenterr/adm-manager/master/mega-center.rar > /dev/null 2>&1
-echo "     CopyRight 2020 Alone in the Dark"
+echo -e "\e[103m\e[32m   CopyRight 2020 Alone in the Dark\e[0m"
 echo
 echo
-echo "INSTALACION EN CURSO.......LA DEMORA DEPENDE DE SU MAQUINA"
+echo -e "\e[103m\e[32mINSTALACION EN CURSO.......LA DEMORA DEPENDE DE SU MAQUINA\e[0m"
 #REPOSITORIOS
 sudo apt-get install nano -y &>/dev/null
 sudo apt-get install bc -y &>/dev/null
 sudo apt-get install screen -y &>/dev/null
 sudo apt-get install python -y &>/dev/null
 echo
-echo "GESTIONANDO DEPENDENCIAS....."
+echo -e "\e[103m\e[32m GESTIONANDO DEPENDENCIAS.....\e[0m"
 sudo apt-get install python3 -y &>/dev/null
 sudo apt-get install curl -y &>/dev/null
 sudo apt-get install ufw -y &>/dev/null
 sudo apt-get install unzip -y &>/dev/null
 sudo apt-get install zip -y &>/dev/null
 echo
-echo "INSTALANDO UPGRADES............."
+echo -e "\e[103m\e[32m INSTALANDO UPGRADES.............\e[0m"
 sudo apt install lsof  -y &>/dev/null
 sudo apt install cowsay -y &>/dev/null
 sudo apt install lolcat -y &>/dev/null
 sudo apt install figlet -y &>/dev/null
 sudo apt install toilet -y &>/dev/null
 sudo apt-get install unrar -y &>/dev/null
+sudo apt-get install unrar-free -y &>/dev/null
 sudo apt install net-tools -y &>/dev/null
 echo
-echo "INSTALANDO BINARIOS.........."
+echo -e "\e[103m\e[32m INSTALANDO BINARIOS..........\e[0m"
 sleep 1s
 apt-get update -y
 apt-get upgrade -y
@@ -44,7 +45,7 @@ mv ETC/B-ADMuser /etc/
 mv ETC/dropbear /etc/ 
 clear
 echo
-echo "CONFIGURANDO PANEL PARA INICIAR........."
+echo -e "\e[103m\e[32mCONFIGURANDO PANEL PARA INICIAR.........\e[0m"
 #PERMISOS
 chmod 777 /etc/bin/ -R
 chmod 777 /etc/newadm/ -R
@@ -61,7 +62,7 @@ rm -r -f USR
 rm -f mega-center.rar
 chmod 777 cargar.sh
 clear
-echo "EL PANEL SE INICIA CON "adm" , luego puede seleccionar inicio automatico....."
+echo -e "\e[103m\e[32mEL PANEL SE INICIA CON "adm" , luego puede seleccionar inicio automatico.....\e[0m"
 echo
 echo "LA INSTALACION FINALIZARA en 5 SEGUNDOS........" 
 sleep 5s
